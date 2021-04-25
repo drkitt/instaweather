a5513c63928d27920bb932338738e2c664421d5b
drkitt <alex.kitt@outlook.com>
2021-04-25 16:09:01 +0100

    Add periodic background worker updates

--- worker_src/c/instant-weather_worker.c
+++ worker_src/c/instant-weather_worker.c
@@ -7,9 +7,25 @@ weather
 // Functions and types for background workers
 #include <pebble_worker.h>
 
+/*
+Called periodically according to the tick timer service. Updates wheather
+info.
+Parameters (which are mostly just here to satisfy the tick timer service's
+interface):
+    tick_time: Contains the current time
+    units_changed: Constant denoting which unit has changed (hour, minute, or second).
+*/
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
+{
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "hey what's up it's the background worker");
+}
+
 /* Initializes the worker */
 static void init() {
     APP_LOG(APP_LOG_LEVEL_DEBUG, "Background worker time >:)");
+
+    // Register with the tick timer service in order to update periodically
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 /* Deinitializes the worker */
