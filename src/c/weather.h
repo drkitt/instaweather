f4d246e737ba365b9e0dc424a97904e303a28af9
drkitt <alex.kitt@outlook.com>
2021-04-25 19:07:17 +0100

    Start weather header

---
+++ src/c/weather.h
@@ -0,0 +1,5 @@
+/*
+Contains functionality for fetching the weather from the OpenWeatherMap API.
+Generally used by the background worker, but may be used by the foreground app
+if there's no weather info saved.
+*/
