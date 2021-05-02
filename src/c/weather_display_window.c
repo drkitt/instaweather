69bdf4924fbf5476881ad16976c3c5591719fee8
drkitt <alex.kitt@outlook.com>
2021-05-02 21:26:32 +0100

    Start weather display window

---
+++ src/c/weather_display_window.c
@@ -0,0 +1,59 @@
+/*
+Contains the implementation for the weather display window
+*/
+
+/* Included files */
+// Weather display window interface
+#include "weather_display_window.h"
+
+/* Static variables */
+// Displays the temperature
+static TextLayer *temperature_layer;
+
+/* Static prototypes */
+// Callback for loading the window
+static void load(Window *window);
+// Callback for unloading the window
+static void unload(Window *window);
+
+/*
+Sets up a window and returns a pointer to it.
+Note that this function doesn't add the new window to the window stack; the
+caller must manage the returned window itself.
+*/
+Window *weather_display_window_create(void) {
+    Window *new_window = window_create();
+    window_set_window_handlers(new_window, (WindowHandlers) {
+        .load = load,
+        .unload = unload,
+    });
+    return new_window;
+}
+
+/*
+Called when the window loads
+
+Parameters:
+    window: Pointer to the window-to-be
+*/
+static void load(Window *window) {
+    // Get the main layer's boundsf
+    Layer *window_layer = window_get_root_layer(window);
+    const GRect bounds = layer_get_bounds(window_layer);
+
+    // Set up temperature display
+    temperature_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
+    text_layer_set_text(temperature_layer, "Hello world!");
+    text_layer_set_text_alignment(temperature_layer, GTextAlignmentCenter);
+    layer_add_child(window_layer, text_layer_get_layer(temperature_layer));
+}
+
+/*
+Called when the window unloads
+
+Parameters:
+    window: Pointer to the window-to-not-be
+*/
+static void unload(Window *window) {
+    text_layer_destroy(temperature_layer);
+}
---
+++ src/c/weather_display_window.h
@@ -0,0 +1,10 @@
+/*
+Contains the interface for the window that displays the weather
+*/
+
+/* Included files */
+// Pebble standard library
+#include <pebble.h>
+
+// Sets up a window and returns a pointer to it
+Window *weather_display_window_create(void);
